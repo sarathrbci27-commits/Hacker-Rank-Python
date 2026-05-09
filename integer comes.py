cae6d8bba5f811fa7ef5315b3a8d25c8117f0bbe
Sarathr <sarathr_bci27@mepcoeng.ac.in>
2026-05-09 14:17:05 +0530

    Add files via upload

---
+++ integer comes.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    d = int(input())
+    
+    result = (a ** b) + (c ** d)  # Calculate the result of a^b + c^d
+    print(result)
