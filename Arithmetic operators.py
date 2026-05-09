9e5c93c9362ef5deea1b847e0cccb44362dde1cd
Sarathr <sarathr_bci27@mepcoeng.ac.in>
2026-05-09 13:58:06 +0530

    Add files via upload

---
+++ Arithmetic operators.py
@@ -0,0 +1,12 @@
+# Read two integers from STDIN
+a = int(input())
+b = int(input())
+
+# Print the sum of the two numbers
+print(a + b)
+
+# Print the difference of the two numbers (first - second)
+print(a - b)
+
+# Print the product of the two numbers
+print(a * b)
